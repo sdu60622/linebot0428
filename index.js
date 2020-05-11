1dfb762701cafd083ac220a210c6854cd1a66a3b
sdu60622 <61406207+sdu60622@users.noreply.github.com>
2020-05-11 16:31:46 +0800

    新增外國貨幣

--- index.js
+++ index.js
@@ -25,20 +25,49 @@ bot.on('message', async (event) => {
     // https://prime.exchangerate-api.com/v5/76b653717686bfbfdd3fb10f/latest/TWD
     const data = await rp({ uri: 'https://tw.rter.info/capi.php', json: true })
     const golddata = await rp({ uri: 'https://metals-api.com/api/latest?access_key=lt0y6y362c39l1k5zs25nff8pub63mftq1sf1dst3h0arb0hlt6w18pjkbzj&base=TWD&symbols=XAU%2CXAG%2CXPD%2CXPT%2CXRH', json: true })
+    var usd = 1 / data.USDTWD.Exrate
+    // ---------------------------------美金-----------------------------------------------------------------
     if (msg.indexOf('usd') !== -1 || msg.indexOf('美金') !== -1 || msg.indexOf('美元') !== -1) {
-      const answer = data.USDTWD.Exrate
-      event.reply('今日的美金匯率是:' + answer)
+      const answer = data.USDTWD.Exrate.toString().match(/^\d+(?:\.\d{0,2})?/)
+      event.reply('今日的美金匯率約:' + answer)
       console.log(answer)
-    } else if (msg.indexOf('jpy') !== -1 || msg.indexOf('日幣') !== -1 || msg.indexOf('日元') !== -1) {
-      const answer = data.conversion_rates.JPY
-      event.reply('今日的日元匯率是:' + answer)
+      // ---------------------------------日幣-----------------------------------------------------------------
+    } else if (msg.indexOf('jpy') !== -1 || msg.indexOf('日幣') !== -1 || msg.indexOf('日元') !== -1 || msg.indexOf('日圓') !== -1) {
+      const answer = (usd * data.USDJPY.Exrate).toString().match(/^\d+(?:\.\d{0,2})?/)
+      event.reply('今日的日元匯率約:' + answer)
       console.log(answer)
+      // ---------------------------------人民幣-----------------------------------------------------------------
     } else if (msg.indexOf('cny') !== -1 || msg.indexOf('人民幣') !== -1) {
-      const answer = data.conversion_rates.CNY
-      event.reply('今日的人民幣匯率是:' + answer)
+      const answer = (usd * data.USDCNY.Exrate).toString().match(/^\d+(?:\.\d{0,2})?/)
+      event.reply('今日的人民幣匯率約:' + answer)
+      console.log(answer)
+      // ---------------------------------港幣-----------------------------------------------------------------
+    } else if (msg.indexOf('hkd') !== -1 || msg.indexOf('港幣') !== -1) {
+      const answer = (usd * data.USDHKD.Exrate).toString().match(/^\d+(?:\.\d{0,2})?/)
+      event.reply('今日的港幣匯率約:' + answer)
+      console.log(answer)
+      // ---------------------------------歐元-----------------------------------------------------------------
+    } else if (msg.indexOf('eur') !== -1 || msg.indexOf('歐元') !== -1) {
+      const answer = (usd * data.USDEUR.Exrate).toString().match(/^\d+(?:\.\d{0,2})?/)
+      event.reply('今日的歐元匯率約:' + answer)
+      console.log(answer)
+      // ---------------------------------澳幣-----------------------------------------------------------------
+    } else if (msg.indexOf('aud') !== -1 || msg.indexOf('澳幣') !== -1) {
+      const answer = (usd * data.USDAUD.Exrate).toString().match(/^\d+(?:\.\d{0,2})?/)
+      event.reply('今日的澳幣匯率約:' + answer)
+      console.log(answer)
+      // ---------------------------------澳幣-----------------------------------------------------------------
+    } else if (msg.indexOf('aud') !== -1 || msg.indexOf('澳幣') !== -1) {
+      const answer = (usd * data.USDAUD.Exrate).toString().match(/^\d+(?:\.\d{0,2})?/)
+      event.reply('今日的澳幣匯率約:' + answer)
+      console.log(answer)
+      // ---------------------------------加幣-----------------------------------------------------------------
+    } else if (msg.indexOf('cad') !== -1 || msg.indexOf('加幣') !== -1) {
+      const answer = (usd * data.USDCAD.Exrate).toString().match(/^\d+(?:\.\d{0,2})?/)
+      event.reply('今日的加幣匯率約:' + answer)
       console.log(answer)
 
-      // ------------------------------貴金屬價格--------------------------------------------------------
+      // ------------------------------貴金屬價格 https://metals-api.com/--------------------------------------------------------
     } else if (msg.indexOf('gold') !== -1 || msg.indexOf('金') !== -1) {
       const answer = golddata.rates.XAU
       event.reply('今日的金價是:' + answer)
@@ -51,6 +80,7 @@ bot.on('message', async (event) => {
     msg = '發生錯誤'
   }
   // event.reply('匯率是:' + answer)
+  console.log(event)
 })
 
 // 在port啟動
